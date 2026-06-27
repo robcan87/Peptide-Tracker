9183566d49643e44b039652e89c6c487ed1b3657
robcan87 <rob_lawson@hotmail.com>
2026-06-27 17:46:55 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "peptide-tracker-v1";
+const CACHE = "peptide-tracker-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
